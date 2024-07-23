78b26c30c3a8207ac00a0e0079e01f6efc126d5f
RasoulZamani <r.zamani72@gmail.com>
2024-07-23 10:11:19 +0330

    load static files

--- pkg/bootstrap/serve.go
+++ pkg/bootstrap/serve.go
@@ -1,13 +1,19 @@
 package bootstrap
 
 import (
+	static "github.com/RasoulZamani/hiGin/pkg"
 	"github.com/RasoulZamani/hiGin/pkg/html"
 	"github.com/RasoulZamani/hiGin/pkg/routing"
 )
 
 func Serve() {
 	routing.Init()
-	routing.RegisterRoutes()
+
+	static.LoadStatic(routing.GetRouter())
+
 	html.LoadHtml(routing.GetRouter())
+
+	routing.RegisterRoutes()
+
 	routing.Serve()
 }
